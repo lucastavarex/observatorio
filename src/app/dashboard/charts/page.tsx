c1fddbe44a0d63b5f94e74cdd7ff73df2cc43705
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2025-07-24 09:34:07 -0300

    progress

--- src/app/dashboard/charts/page.tsx
+++ src/app/dashboard/charts/page.tsx
@@ -1,13 +1,11 @@
 "use client"
 
-import { InputWithIcon } from "@/components/ui/input"
-import { Search } from "lucide-react"
 import React from "react"
-import { DataTable } from "./components/data-table"
-import { FiltersSidebar } from "./components/filters-sidebar"
-import { useDashboardData } from "./hooks/use-dashboard-data"
+import { DataTable } from "../components/data-table"
+import { FiltersSidebar } from "../components/filters-sidebar"
+import { useDashboardData } from "../hooks/use-dashboard-data"
 
-export default function DashboardContent() {
+export default function ChartsPage() {
   const [selectedFilter, setSelectedFilter] = React.useState<string>("Terminais Rodoviários")
   const [globalFilter, setGlobalFilter] = React.useState<string>("")
   const { data, stats, isLoading } = useDashboardData(selectedFilter)
@@ -26,23 +24,17 @@ export default function DashboardContent() {
             />
           </div>
         </div>
+        <div className="bg-white w-full min-w-0 lg:min-w-[350px] lg:w-[350px] lg:flex-shrink-0 rounded-lg min-h-[200px] lg:min-h-full overflow-hidden">
+          <div className="h-full overflow-y-auto">
+            <FiltersSidebar 
+              selectedFilter={selectedFilter}
+              onFilterChange={setSelectedFilter}
+            />
+          </div>
+        </div>
         
         {/* Container for search and data table */}
         <div className="flex flex-col gap-4 lg:flex-1">
-          {/* Green container - Search Input */}
-          <div className="rounded-lg flex items-center ">
-            <div className="flex items-center justify-between w-full">
-              {/* <h1 className="text-lg font-semibold ">Dashboard</h1> */}
-              <InputWithIcon
-                icon={<Search className="h-5 w-5 lg:mr-4 text-black" />}
-                iconPosition="right"
-                placeholder="Buscar por Município ou Unidade Federativa ..."
-                value={globalFilter}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setGlobalFilter(event.target.value)}
-                className="bg-white placeholder:text-black text-md font-medium px-4 lg:px-9 h-14 border-none shadow-none w-full focus-visible:ring-0 focus-visible:outline-none focus:ring-0 focus:outline-none"
-              />
-            </div>
-          </div>
           
           {/* Blue container - DataTable */}
           <div className="bg-white rounded-lg min-h-[200px]">
