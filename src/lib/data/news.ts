6b4b7e9c4d5a0c562954c2393bb55185877e86bc
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2025-09-14 12:41:46 -0300

    feat: progress

--- src/lib/data/news.ts
+++ src/lib/data/news.ts
@@ -1,86 +1,236 @@
 import video1 from "@/app/assets/images/video1.png"
+import video10 from "@/app/assets/images/video10.png"
+import video11 from "@/app/assets/images/video11.png"
+import video12 from "@/app/assets/images/video12.png"
+import video13 from "@/app/assets/images/video13.png"
+import video14 from "@/app/assets/images/video14.png"
+import video15 from "@/app/assets/images/video15.png"
+import video16 from "@/app/assets/images/video16.png"
+import video17 from "@/app/assets/images/video17.png"
+import video18 from "@/app/assets/images/video18.png"
+import video19 from "@/app/assets/images/video19.png"
 import video2 from "@/app/assets/images/video2.png"
+import video21 from "@/app/assets/images/video21.png"
+import video22 from "@/app/assets/images/video22.png"
+import video23 from "@/app/assets/images/video23.png"
 import video3 from "@/app/assets/images/video3.png"
 import video4 from "@/app/assets/images/video4.png"
 import video5 from "@/app/assets/images/video5.png"
 import video6 from "@/app/assets/images/video6.png"
 import video7 from "@/app/assets/images/video7.png"
 import video8 from "@/app/assets/images/video8.png"
+import video9 from "@/app/assets/images/video9.png"
 import { NewsItem } from "@/lib/types/news"
 
 export const newsData: NewsItem[] = [
     {
+    id: 1,
+    title: "Os caminhos para financiar a adaptação climática no Brasil",
+    source: "Insper",
+    date: "2025",
+    image: video1.src,
+    link: "https://www.insper.edu.br/pt/conteudos/cidades/os-caminhos-para-financiar-a-adaptacao-climatica-no-brasil",
+    apareceNaHome: false
+  },
+    {
+    id: 2,
+    title: "Insper e iFood detalham acordo que promove pesquisas com impacto social",
+    source: "Insper",
+    date: "2025",
+    image: video2.src,
+    link: "https://www.insper.edu.br/pt/conteudos/cidades/insper-e-ifood-detalham-acordo-que-promove-pesquisas-com-impacto-social",
+    apareceNaHome: false
+  },
+    {
     id: 3,
+    title: "Coalizão propõe plano para reduzir em 70% as emissões do transporte até 2050",
+    source: "Insper",
+    date: "2025",
+    image: video3.src,
+    link: "https://www.insper.edu.br/pt/conteudos/cidades/coalizao-propoe-plano-para-reduzir-em-70-as-emissoes-do-transporte-ate-2050",
+    apareceNaHome: false
+  },
+    {
+    id: 4,
+    title: "Insper lança a Rede de Cidades sobre Mobilidade Sustentável",
+    source: "Diário de Transporte",
+    date: "2025",
+    image: video4.src,
+    link: "https://diariodotransporte.com.br/2025/04/26/observatorio-nacional-de-mobilidade-sustentavel-lanca-rede-de-cidades-com-foco-em-colaboracao-e-dados-abertos/",
+    apareceNaHome: false
+  },
+    {
+    id: 5,
+    title: "Insper lança a Rede de Cidades sobre Mobilidade Sustentável",
+    source: "Mobilize Brasil",
+    date: "2025",
+    image: video5.src,
+    link: "https://www.mobilize.org.br/noticias/14309/insper-lanca-a-rede-de-cidades-sobre-mobilidade-sustentavel.html",
+    apareceNaHome: false
+  },
+    {
+    id: 6,
+    title: "Insper lança a Rede de Cidades sobre Mobilidade Sustentável",
+    source: "Casa e Mercado",
+    date: "2025",
+    image: video6.src,
+    link: "https://casaemercado.com.br/insper-lanca-rede-de-cidades-do-observatorio-nacional-de-mobilidade-sustentavel/",
+    apareceNaHome: false
+  },
+    {
+    id: 7,
+    title: "Sexta da Mobilidade discute temas atuais, como a queda do total de viagens em São Paulo",
+    source: "Insper",
+    date: "2025",
+    image: video7.src,
+    link: "https://www.insper.edu.br/pt/conteudos/cidades/sexta-da-mobilidade-discute-temas-atuais-como-a-queda-do-total-de-viagens-em-sao-paulo",
+    apareceNaHome: false
+  },
+  {
+    id: 8,
+    title: "Contra a visão “carrocêntrica” das cidades",
+    source: "Insper",
+    date: "2025",
+    image: video8.src,
+    link: "https://www.insper.edu.br/pt/conteudos/cidades/contra-a-visao-carrocentrica-das-cidades",
+    apareceNaHome: false
+  },
+  {
+    id: 9,
     title: "Observatório Nacional de Mobilidade Sustentável firma acordo de cooperação com o Rio de Janeiro",
     source: "Technibus",
     date: "2025",
-    image: video3.src,
+    image: video9.src,
     link: "https://technibus.com.br/2025/02/19/observatorio-nacional-de-mobilidade-sustentavel-firma-acordo-de-cooperacao-com-o-rio-de-janeiro/",
     apareceNaHome: true
   },
-   {
-    id: 8,
+  {
+    id: 10,
+    title: "Observatório Nacional de Mobilidade Sustentável, parceria entre Insper e Grupo CCR, firma acordo de cooperação com o Rio de Janeiro",
+    source: "Conexão In",
+    date: "2025",
+    image: video10.src,
+    link: "https://conexaoin.com.br/observatorio-nacional-de-mobilidade-sustentavel-parceria-entre-insper-e-grupo-ccr-firma-acordo-de-cooperacao-com-o-rio-de-janeiro/",
+    apareceNaHome: false
+  },
+  {
+    id: 11,
     title: "Agenda da descarbonização pode ser melhor",
     source: "ABOL",
     date: "2025",
-    image: video8.src,
+    image: video11.src,
     link: "https://abolbrasil.org.br/noticias/noticias-do-setor/agenda-da-descarbonizacao-pode-ser-melhor",
     apareceNaHome: false
   },
   {
-    id: 4,
-    title: "Sexta da mobilidade: Soluções baseadas na natureza e mobilidade urbana: a urgência da mudança de paradigma das cidades",
-    source: "Insper",
-    date: "2024",
-    image: video4.src,
-    link: "https://www.insper.edu.br/pt/eventos/2024/08/sexta-da-mobilidade-solucoes-baseadas-na-natureza-e-mobilidade-urbana-a-urgencia-da-mudanca-de-paradigma-das-cidades",
-    apareceNaHome: true
+    id: 12,
+    title: "Moradores de São Paulo têm se deslocado menos e, principalmente, de forma individual, aponta pesquisa",
+    source: "CBN",
+    date: "2025",
+    image: video12.src,
+    link: "https://cbn.globo.com/sao-paulo/noticia/2025/02/11/moradores-de-sao-paulo-tem-se-deslocado-menos-e-principalmente-de-forma-individual-aponta-pesquisa.ghtml",
+    apareceNaHome: false
   },
   {
-    id: 5,
-    title: "Sexta da mobilidade: Dados e Mobilidade Urbana",
+    id: 13,
+    title: "Dados do iFood sobre as atividades dos entregadores passam a ser armazenados no Insper",
     source: "Insper",
     date: "2024",
-    image: video5.src,
-    link: "https://www.insper.edu.br/pt/eventos/2024/10/sexta-da-mobilidade-dados-e-mobilidade-urbana",
-    apareceNaHome: true
+    image: video13.src,
+    link: "https://www.insper.edu.br/pt/conteudos/cidades/dados-do-ifood-sobre-as-atividades-dos-entregadores-passam-a-ser-armazenados-no-insper",
+    apareceNaHome: false
   },
   {
-    id: 6,
-    title: "Sexta da Mobilidade: Soluções Brasileiras para o Futuro da Mobilidade, o Caso do Aeromóvel",
+    id: 14,
+    title: "Professores do Insper visitam modelos de urbanismo social na Colômbia",
+    source: "Insper",
+    date: "2025",
+    image: video14.src,
+    link: "https://www.insper.edu.br/pt/conteudos/cidades/professores-do-insper-visitam-modelos-de-urbanismo-social-na-colombia",
+    apareceNaHome: false
+  },
+  {
+    id: 15,
+    title: "Os desafios do financiamento do transporte público  ",
+    source: "Insper",
+    date: "2025",
+    image: video15.src,
+    link: "https://www.insper.edu.br/pt/conteudos/cidades/os-desafios-do-financiamento-do-transporte-publico",
+    apareceNaHome: false
+  },
+  {
+    id: 16,
+    title: "Ciclo de webinários debaterá desafios e oportunidades para os municípios",
     source: "Insper",
     date: "2024",
-    image: video6.src,
-    link: "https://www.insper.edu.br/pt/eventos/2024/11/sexta-da-mobilidade-solucoes-brasileiras-para-o-futuro-da-mobilidade-o-caso-do-aeromovel?utm_content=316378986&utm_medium=social&utm_source=facebook&hss_channel=fbp-120253158027896",
-    apareceNaHome: true
+    image: video16.src,
+    link: "https://www.insper.edu.br/pt/conteudos/cidades/ciclo-de-webinarios-debatera-desafios-e-oportunidades-para-os-municipios",
+    apareceNaHome: false
   },
   {
-    id: 7,
-    title: "Evento de Lançamento do Guia de Eletromobilidade para Cidades Brasileiras",
+    id: 17,
+    title: "Centro de Estudos das Cidades lança o Guia de Eletromobilidade para Cidades Brasileiras: Transporte Público",
     source: "Insper",
     date: "2024",
-    image: video7.src,
-    link: "https://www.insper.edu.br/pt/eventos/2024/09/mobilidade-sustentavel-na-agenda-da-cop-30-impactos-no-meio-ambiente-e-mudanca-climatica",
-    apareceNaHome: true
+    image: video17.src,
+    link: "https://www.insper.edu.br/pt/conteudos/cidades/centro-de-estudos-das-cidades-lanca-o-guia-de-eletromobilidade-para-cidades-brasileiras-transporte-publico",
+    apareceNaHome: false
   },
   {
-    id: 1,
+    id: 18,
+    title: "Sérgio Avelleda lança “Nas trincheiras da Mobilidade”",
+    source: "Insper",
+    date: "2024",
+    image: video18.src,
+    link: "https://www.insper.edu.br/pt/conteudos/politicas-publicas/sergio-avelleda-lanca-nas-trincheiras-da-mobilidade",
+    apareceNaHome: false
+  },
+  {
+    id: 19,
+    title: "“Cidade inteligente não precisa ter drones, robôs ou milhares de sensores”",
+    source: "Insper",
+    date: "2024",
+    image: video19.src,
+    link: "https://www.insper.edu.br/pt/noticias/2023/7/-cidade-inteligente-nao-precisa-ter-drones--robos-ou-milhares-de",
+    apareceNaHome: false
+  },
+  {
+    id: 20,
+    title: "Mobilidade urbana em São Paulo: caminhos para uma cidade sustentável",
+    source: "Insper",
+    date: "2024",
+    image: video18.src,
+    link: "https://www.insper.edu.br/pt/noticias/2022/12/-quem-so-anda-de-carro-tambem-deveria-pagar-pelo-transporte-publ",
+    apareceNaHome: false
+  },
+  {
+    id: 21,
+    title: "Seis dicas para curtir a experiência de pedalar em São Paulo",
+    source: "Insper",
+    date: "2023",
+    image: video21.src,
+    link: "https://www.insper.edu.br/pt/noticias/2023/7/seis-dicas-para-curtir-a-experiencia-de-pedalar-em-sao-paulo",
+    apareceNaHome: false
+  },
+  {
+    id: 22,
     title: "Grupo CCR e Laboratório Arq. Futuro de Cidades lançam Observatório Nacional de Mobilidade Sustentável",
     source: "Estadão",
     date: "2023",
-    image: video1.src,
+    image: video22.src,
     link: "https://mobilidade.estadao.com.br/mobilidade-para-que/grupo-ccr-e-laboratorio-arq-futuro-de-cidades-lancam-observatorio-nacional-de-mobilidade-sustentavel",
     apareceNaHome: true
   },
   {
-    id: 2,
-    title: "Insper e CCR lançam Observatório Nacional de Mobilidade Sustentável",
+    id: 23,
+    title: "Evento de Lançamento do Guia de Eletromobilidade para Cidades Brasileiras",
     source: "Insper",
     date: "2023",
-    image: video2.src,
-    link: "https://www.insper.edu.br/noticias/insper-e-ccr-lancam-observatorio-nacional-de-mobilidade-sustentavel/",
-    apareceNaHome: false
+    image: video23.src,
+    link: "https://www.insper.edu.br/pt/eventos/2024/09/mobilidade-sustentavel-na-agenda-da-cop-30-impactos-no-meio-ambiente-e-mudanca-climatica",
+    apareceNaHome: true
   },
+
 ]
 
 // Função para obter notícias que aparecem na home
