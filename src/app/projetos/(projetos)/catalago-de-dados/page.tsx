fbad8af9a0b6f9fb7db9a86b52070eee72930b2f
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2025-11-15 15:10:06 -0300

    fix 3

--- src/app/projetos/(projetos)/catalago-de-dados/page.tsx
+++ src/app/projetos/(projetos)/catalago-de-dados/page.tsx
@@ -6,6 +6,12 @@ export default function CatalogoDeDadosPage() {
     <div className="flex flex-col gap-4 pt-4 bg-[#f9f9f6] pb-30">
       <CatalogoDeDadosCidadesParceiras />
       <CatalogoDeDadosCidadesComDadosAbertos />
+      <div className="mt-8 p-6 mx-4 2xl:mx-16 bg-white shadow-sm rounded-lg border-l-4 border-primary">
+              <p className="text-gray-700">
+               As cidades sinalizadas com <span className="font-bold text-lg">*</span> fazem parte do acordo firmado com o Consórcio da Região Metropolitana do Recife para o compartilhamento de dados de mobilidade urbana.
+               
+              </p>
+            </div>
     </div>
   )
 }
