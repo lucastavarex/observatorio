d1ead39bb8879c95b47a8ce4311d337eb7b88733
Lucas Tavares <lucastavarestt@gmail.com>
2026-09-01 19:56:01 -0300

    chore: update Next.js configuration to conditionally set output mode
    - Modified the Next.js configuration to set the output mode to 'standalone' based on the BUILD_STANDALONE environment variable, enhancing build flexibility.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: process.env.BUILD_STANDALONE === 'false' ? undefined : 'standalone',
 };
 
 export default nextConfig;
